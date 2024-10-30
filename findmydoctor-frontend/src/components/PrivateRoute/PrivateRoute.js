991e7c26bf5dd8ca02fb67a59969766ec13aaa05
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2024-10-30 17:09:19 +0530

    the refresh issue is solved and it was the issue with the private route

--- findmydoctor-frontend/src/components/PrivateRoute/PrivateRoute.js
+++ findmydoctor-frontend/src/components/PrivateRoute/PrivateRoute.js
@@ -4,8 +4,12 @@ import { useSelector } from 'react-redux';
 
 const PrivateRoute = ({ children }) => {
     const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const accessToken = localStorage.getItem('access_token'); // Get the access token from local storage
 
-    return isAuthenticated ? children : <Navigate to="/" />;
+    // Check if the user is authenticated via Redux or has a valid access token in local storage
+    const isUserAuthenticated = isAuthenticated || !!accessToken;
+
+    return isUserAuthenticated ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
