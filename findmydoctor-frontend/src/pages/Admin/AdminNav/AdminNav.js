c1b48d8b593cc0f428fa918a546f5dc32ce40332
Aswin v gopal <aswinvazhathodiyil113@gmail.com>
2024-11-13 15:29:14 +0530

    admin side filtering and pagination added

---
+++ findmydoctor-frontend/src/pages/Admin/AdminNav/AdminNav.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './AdminNav.css';
+import logo from '../../../Images/icon.svg';
+import { useNavigate } from 'react-router-dom';
+
+const AdminNav = () => {
+    const navigate = useNavigate();
+    
+    const handleNavigate = (path) => {
+        navigate(path);
+    };
+
+    return (
+        <nav className="navbar">
+            <div className="navbar-logo">
+                <img src={logo} alt="Find My Doctor Logo" />
+                <span>FIND <span className="highlight">MY</span> DOCTOR</span>
+            </div>
+        </nav>
+    );
+};
+
+export default AdminNav;
